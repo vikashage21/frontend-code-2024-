ec8639060d12a98e079735d1dc3c123091055a98
vikash sharma <vk220783@gmail.com>
2024-04-01 13:26:20 +0530

    created a navbar in this website of refokus

---
+++ projects/project13/refocus website/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className=' w-full h-screen bg-zinc-900 text-zinc-50'>
+      <Navbar />
+      
+    </div>
+  )
+}
+
+export default App
